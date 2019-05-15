942caaaccd66cebaad6fd29c79a9581809df4ac4
Phạm Tấn Phát <22995627+phamtanphat@users.noreply.github.com>
2019-05-15 19:26:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,89 @@
 const express = require('express');
 const app = express();
-const{json} = require('body-parser');
+const {json} = require('body-parser');
 const mysql = require('mysql');
 
-// yarn add cors xu ly cho cross port
-app.listen('3000' , () => console.log("Server started"));
+
+app.use(json());
+
+const con = mysql.createPool({
+  connectionLimit : 50,
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'quanlytuvung',
+});  
+
+app.get('/words' , function(req,res){
+    con.getConnection((error , connection) =>{
+        if(error){
+            connection.release();
+            res.send({success : false , message : error})
+        }else{
+            connection.query("SELECT * FROM tuvung" , function (error, rows, fields) {
+                connection.release();
+                if (error) return res.send({success : false , message : "ERROR VALUE"});
+                res.send({success : true , words : rows});
+            });
+        }
+    })
+});
+app.post('/word' ,(req,res) =>{
+    con.getConnection((error , connection) =>{
+        if(error){
+            connection.release();
+            res.send({success : false , message : error})
+        }else{
+            const {en , vn} = req.body;  
+            const word = {Id : null , en , vn , isMemorized : 0}
+            connection.query("INSERT INTO tuvung SET ?",word, function (error, result, fields) {
+                connection.release();
+                if (!!error){
+                    res.send({success : false , message : error.message});
+                }else{
+                    res.send({success : true , row : result.affectedRows})
+                }
+            });
+        }
+
+    });
+})
+app.put('/word/:id',(req,res) => {
+    con.getConnection((error , connection) =>{
+        if(error){
+            connection.release();
+            res.send({success : false , message : error})
+        }else{
+            const {id} = req.params;
+            const{isMemorized} = req.body;  
+            connection.query("UPDATE tuvung SET isMemorized = ? WHERE Id = ?", [isMemorized,id] , function (error, result, fields) {
+                connection.release();
+                if (!!error){
+                    res.send({success : false , message : error.message});
+                }else{
+                    res.send({success : true , row : result.affectedRows})
+                }
+            });
+        }
+    });
+});
+app.delete('/word/:id' , (req,res)=>{
+    con.getConnection((error , connection) =>{
+        if(error){
+            connection.release();
+            res.send({success : false , message : error})
+        }else{
+            const{id} = req.params;  
+            connection.query("DELETE FROM tuvung WHERE Id = ?", [id] , function (error, result, fields) {
+                connection.release();
+                if (!!error) return res.send({success : false , message : error.message});
+                res.send({success : true , row : result.affectedRows})
+            });
+        }
+      
+    });
+
+
+});
+
+app.listen('3000', () => console.log("Server started"));
